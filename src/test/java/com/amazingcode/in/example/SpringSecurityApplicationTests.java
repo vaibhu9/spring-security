a0ad347fa5f747ccc4c93541f9f1e454aaaebea1
vaibhu9 <vaibhavshejol9@gmail.com>
2024-09-18 23:50:05 +0530

    project created

---
+++ src/test/java/com/amazingcode/in/example/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazingcode.in.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
